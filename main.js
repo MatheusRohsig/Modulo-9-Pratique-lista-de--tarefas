339bc23e6c2b9247ba39de7b07d7d260c759cea8
Matheus Rohsig <matheusrohsig@hotmail.com>
2024-02-21 23:53:24 -0300

    Terceiro commit - acrescimo da função de riscar itens da lista

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function() {  //Necessario  para inicializar o jquery
 
-    $('header button').click(function() {
+    $('header button').click(function() {   // Faz com que o formilario desça quando o botão do cabeçalho é acionado
         $('form').slideDown();
     })
 
-    $('#botao-cancelar').click(function() {
+    $('#botao-cancelar').click(function() {  // Faz com que o formulário suba quando o botão cancelar é acionado
         $('form').slideUp();
     })
 
 
 
-    $('form').on('submit', function(e) {
+    $('form').on('submit', function(e) {  //Evita o comportamento padrão do formulário de enviar os dados e atualizar a página
         e.preventDefault();
 
         // Obter o valor da nova tarefa do campo de entrada
@@ -27,6 +27,12 @@ $(document).ready(function() {
 
         // Limpar o campo de entrada após adicionar a tarfa
         $('#tarefa-acrescentada').val('')
+
+
+        // sempre que clicar em um "li" vai colocar o traço
+        $('ul').on('click', 'li', function(e){
+            $(this).css('text-decoration', 'line-through');
+        })
 
         
 
